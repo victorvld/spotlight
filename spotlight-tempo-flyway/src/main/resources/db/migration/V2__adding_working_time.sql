cee7609f3d14fed600eee6a4e09632c70666565f
Varranz <varranz.devs@outlook.com>
2023-10-09 17:23:30 +0200

    LIG-20 : Address requested changes in CR

--- spotlight-tempo-flyway/src/main/resources/db/migration/V2__adding_working_time.sql
+++ spotlight-tempo-flyway/src/main/resources/db/migration/V2__adding_working_time.sql
@@ -6,4 +6,3 @@ CREATE TABLE msvc_tempo.working_time (
 
 INSERT INTO msvc_tempo.working_time (working_time_id) VALUES ('1');
 INSERT INTO msvc_tempo.focus_unit (entity_id, user_id, working_time_id) VALUES ('4', '2', '1');
-
