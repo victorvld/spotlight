79b72a833dbba920ef69346aa990f5f39ca0af7d
Varranz <varranz.devs@outlook.com>
2023-10-31 12:50:59 +0100

    LK-21 : Refactor Controller tests

--- spotlight-time/src/test/java/os/psy/research/spotlight/controller/FocusUnitControllerTest.java
+++ spotlight-time/src/test/java/os/psy/research/spotlight/controller/FocusUnitControllerTest.java
@@ -28,7 +28,7 @@ import os.psy.research.spotlight.presentation.mapper.WorkingTimeMapper;
 import os.psy.research.spotlight.testDataFactory.EntityObjectMother;
 import os.psy.research.spotlight.testDataFactory.RequestObjectMother;
 
-import java.util.Collections;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.mockito.Mockito.times;
@@ -57,57 +57,34 @@ public class FocusUnitControllerTest {
         public final String url = "/rest/os.psy.research.spotlight.v1.FocusUnitApi/units";
 
         @Test
-        void whenValidInput_thenReturns200() throws Exception {
+        void whenGetBoardRequestPassValidationRulesAndMediaTypesThenReturns200() throws Exception {
             var userId = "user-uuid";
-            var request = GetFocusUnitsRequest.builder().userId(userId).build();
             var captor = ArgumentCaptor.forClass(String.class);
-            var unit = EntityObjectMother.complete().build();
-            when(mockService.getFocusUnits(userId)).thenReturn(Collections.singletonList(unit));
+            var mockResponse = EntityObjectMother.complete().build();
+            var content = mapper.writeValueAsString(GetFocusUnitsRequest.builder().userId(userId).build());
+            var request = get(url).accept(MediaType.APPLICATION_JSON_VALUE).contentType(MediaType.APPLICATION_JSON_VALUE).content(content);
+            when(mockService.getFocusUnits(userId)).thenReturn(List.of(mockResponse));
 
-            mockMvc.perform(get(url).accept(MediaType.APPLICATION_JSON_VALUE)
-                         .contentType(MediaType.APPLICATION_JSON_VALUE)
-                            .content(mapper.writeValueAsString(request)))
-                    .andExpect(status().isOk());
+            mockMvc.perform(request).andExpect(status().isOk());
 
             verify(mockService, times(1)).getFocusUnits(captor.capture());
             Assertions.assertEquals(userId, captor.getValue());
         }
 
-        static Stream<Arguments> badRequestProvider() {
+        @ParameterizedTest
+        @MethodSource("badRequestsProvider")
+        void whenIncomingRequestFieldsDoNotPassValidationRulesThenReturns400(GetFocusUnitsRequest content) throws Exception {
+            var json = mapper.writeValueAsString(content);
+            var request = get(url).accept(MediaType.APPLICATION_JSON_VALUE).contentType(MediaType.APPLICATION_JSON_VALUE).content(json);
+
+            mockMvc.perform(request).andExpect(status().isBadRequest());
+        }
+
+        static Stream<Arguments> badRequestsProvider() {
             return Stream.of(
                     Arguments.of(GetFocusUnitsRequest.builder().userId(" ").build()),
                     Arguments.of(GetFocusUnitsRequest.builder().build())
             );
-        }
-
-        @ParameterizedTest
-        @MethodSource("badRequestProvider")
-        void whenIncomingRequestFieldsDoNotPassValidationRulesBadRequestIsReturned(GetFocusUnitsRequest request) throws Exception {
-            mockMvc.perform(get(url).accept(MediaType.APPLICATION_JSON_VALUE)
-                            .contentType(MediaType.APPLICATION_JSON_VALUE)
-                            .content(mapper.writeValueAsString(request)))
-                    .andExpect(status().isBadRequest());
-        }
-
-        @Test
-        void whenInvalidInput_thenReturns400() throws Exception {
-            mockMvc.perform(get(url).accept(MediaType.APPLICATION_JSON_VALUE)
-                            .contentType(MediaType.APPLICATION_JSON_VALUE))
-                    .andExpect(status().isBadRequest());
-        }
-
-        @Test
-        void whenInvalidContentType_thenReturns415() throws Exception {
-            mockMvc.perform(get(url).accept(MediaType.APPLICATION_JSON_VALUE)
-                            .contentType(MediaType.APPLICATION_ATOM_XML))
-                    .andExpect(status().isUnsupportedMediaType());
-        }
-
-        @Test
-        void whenInvalidAcceptableType_thenReturns406() throws Exception {
-            mockMvc.perform(get(url).accept(MediaType.APPLICATION_ATOM_XML)
-                            .contentType(MediaType.APPLICATION_JSON_VALUE))
-                    .andExpect(status().isNotAcceptable());
         }
     }
 
@@ -119,38 +96,46 @@ public class FocusUnitControllerTest {
 
         @Test
         void whenValidInput_thenReturns200() throws Exception {
-            var request = RequestObjectMother.RegisterFocusUnit.complete().build();
+            var content = RequestObjectMother.RegisterFocusUnit.complete().build();
             var captor = ArgumentCaptor.forClass(FocusUnit.class);
+            var json = mapper.writeValueAsString(content);
+            var request = post(url).contentType(MediaType.APPLICATION_JSON).content(json);
 
-            mockMvc.perform(post(url)
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(mapper.writeValueAsString(request)))
-                    .andExpect(status().isOk());
+            mockMvc.perform(request).andExpect(status().isOk());
 
             verify(mockService, times(1)).registerFocusUnit(captor.capture());
             Assertions.assertNull(captor.getValue().getEntityId());
-            Assertions.assertEquals(request.userId(), captor.getValue().getUserId());
-            Assertions.assertEquals(request.linkedTaskId(), captor.getValue().getLinkedTaskId());
+            Assertions.assertEquals(content.userId(), captor.getValue().getUserId());
+            Assertions.assertEquals(content.linkedTaskId(), captor.getValue().getLinkedTaskId());
 
-            Assertions.assertTrue(request.workingTimeDto().startedAt().isEqual(captor.getValue().getWorkingTime().getStartedAt()));
-            Assertions.assertTrue(request.workingTimeDto().completedAt().isEqual(captor.getValue().getWorkingTime().getCompletedAt()));
-            Assertions.assertEquals(request.workingTimeDto().plannedMinutes(), captor.getValue().getWorkingTime().getPlannedMinutes());
-            Assertions.assertEquals(request.workingTimeDto().interruptionsDto().get(0).type(), captor.getValue().getWorkingTime().getInterruptions().get(0).getType());
-            Assertions.assertEquals(request.workingTimeDto().interruptionsDto().get(0).reason(), captor.getValue().getWorkingTime().getInterruptions().get(0).getReason());
-            Assertions.assertEquals(request.workingTimeDto().interruptionsDto().get(0).recordedAt(), captor.getValue().getWorkingTime().getInterruptions().get(0).getRecordedAt());
+            Assertions.assertTrue(content.workingTimeDto().startedAt().isEqual(captor.getValue().getWorkingTime().getStartedAt()));
+            Assertions.assertTrue(content.workingTimeDto().completedAt().isEqual(captor.getValue().getWorkingTime().getCompletedAt()));
+            Assertions.assertEquals(content.workingTimeDto().plannedMinutes(), captor.getValue().getWorkingTime().getPlannedMinutes());
+            Assertions.assertEquals(content.workingTimeDto().interruptionsDto().get(0).type(), captor.getValue().getWorkingTime().getInterruptions().get(0).getType());
+            Assertions.assertEquals(content.workingTimeDto().interruptionsDto().get(0).reason(), captor.getValue().getWorkingTime().getInterruptions().get(0).getReason());
+            Assertions.assertEquals(content.workingTimeDto().interruptionsDto().get(0).recordedAt(), captor.getValue().getWorkingTime().getInterruptions().get(0).getRecordedAt());
 
-            Assertions.assertTrue(request.breakTimeDto().startedAt().isEqual(captor.getValue().getBreakTime().getStartedAt()));
-            Assertions.assertTrue(request.breakTimeDto().completedAt().isEqual(captor.getValue().getBreakTime().getCompletedAt()));
-            Assertions.assertEquals(request.breakTimeDto().plannedMinutes(), captor.getValue().getBreakTime().getPlannedMinutes());
-            Assertions.assertEquals(request.breakTimeDto().interruptionsDto().get(0).type(), captor.getValue().getBreakTime().getInterruptions().get(0).getType());
-            Assertions.assertEquals(request.breakTimeDto().interruptionsDto().get(0).reason(), captor.getValue().getBreakTime().getInterruptions().get(0).getReason());
-            Assertions.assertEquals(request.breakTimeDto().interruptionsDto().get(0).recordedAt(), captor.getValue().getBreakTime().getInterruptions().get(0).getRecordedAt());
+            Assertions.assertTrue(content.breakTimeDto().startedAt().isEqual(captor.getValue().getBreakTime().getStartedAt()));
+            Assertions.assertTrue(content.breakTimeDto().completedAt().isEqual(captor.getValue().getBreakTime().getCompletedAt()));
+            Assertions.assertEquals(content.breakTimeDto().plannedMinutes(), captor.getValue().getBreakTime().getPlannedMinutes());
+            Assertions.assertEquals(content.breakTimeDto().interruptionsDto().get(0).type(), captor.getValue().getBreakTime().getInterruptions().get(0).getType());
+            Assertions.assertEquals(content.breakTimeDto().interruptionsDto().get(0).reason(), captor.getValue().getBreakTime().getInterruptions().get(0).getReason());
+            Assertions.assertEquals(content.breakTimeDto().interruptionsDto().get(0).recordedAt(), captor.getValue().getBreakTime().getInterruptions().get(0).getRecordedAt());
 
-            Assertions.assertEquals(request.userAssessmentDto().mood(), captor.getValue().getUserAssessment().getMood());
-            Assertions.assertEquals(request.userAssessmentDto().feedback(), captor.getValue().getUserAssessment().getFeedback());
+            Assertions.assertEquals(content.userAssessmentDto().mood(), captor.getValue().getUserAssessment().getMood());
+            Assertions.assertEquals(content.userAssessmentDto().feedback(), captor.getValue().getUserAssessment().getFeedback());
         }
 
-        static Stream<Arguments> badRequestProvider() {
+        @ParameterizedTest
+        @MethodSource("badRequestsProvider")
+        void whenIncomingRequestFieldsDoNotPassValidationRulesThenReturns400(RegisterFocusUnitRequest content) throws Exception {
+            var json = mapper.writeValueAsString(content);
+            var request = post(url).accept(MediaType.APPLICATION_JSON_VALUE).contentType(MediaType.APPLICATION_JSON_VALUE).content(json);
+
+            mockMvc.perform(request).andExpect(status().isBadRequest());
+        }
+
+        static Stream<Arguments> badRequestsProvider() {
             return Stream.of(
                     Arguments.of(RequestObjectMother.RegisterFocusUnit.complete().userId(" ").build()),
                     Arguments.of(RequestObjectMother.RegisterFocusUnit.complete().userId(null).build()),
@@ -162,36 +147,5 @@ public class FocusUnitControllerTest {
                     Arguments.of(RequestObjectMother.RegisterFocusUnit.complete().breakTimeDto(BreakTimeDto.builder().build()).build())
             );
         }
-
-        @ParameterizedTest
-        @MethodSource("badRequestProvider")
-        void whenIncomingRequestFieldsDoNotPassValidationRulesBadRequestIsReturned(RegisterFocusUnitRequest request) throws Exception {
-            mockMvc.perform(post(url).accept(MediaType.APPLICATION_JSON_VALUE)
-                            .contentType(MediaType.APPLICATION_JSON_VALUE)
-                            .content(mapper.writeValueAsString(request)))
-                    .andExpect(status().isBadRequest());
-        }
-
-        @Test
-        void whenInvalidInput_thenReturns400() throws Exception {
-            mockMvc.perform(post(url).accept(MediaType.APPLICATION_JSON_VALUE)
-                            .contentType(MediaType.APPLICATION_JSON_VALUE))
-                    .andExpect(status().isBadRequest());
-        }
-
-        @Test
-        void whenInvalidContentType_thenReturns415() throws Exception {
-            mockMvc.perform(post(url).accept(MediaType.APPLICATION_JSON_VALUE)
-                            .contentType(MediaType.APPLICATION_ATOM_XML))
-                    .andExpect(status().isUnsupportedMediaType());
-        }
-
-        @Test
-        void whenInvalidAcceptableType_thenReturns406() throws Exception {
-            mockMvc.perform(post(url).accept(MediaType.APPLICATION_ATOM_XML)
-                            .contentType(MediaType.APPLICATION_JSON_VALUE))
-                    .andExpect(status().isNotAcceptable());
-        }
-
     }
 }
