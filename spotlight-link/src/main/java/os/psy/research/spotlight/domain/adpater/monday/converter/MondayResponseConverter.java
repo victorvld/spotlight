72ea0d9d20467a6cdd7edde3ccd1afbc98309064
Varranz <varranz.devs@outlook.com>
2023-10-31 17:57:32 +0100

    LK-22 : Implement Monday Adapter Implementation

---
+++ spotlight-link/src/main/java/os/psy/research/spotlight/domain/adpater/monday/converter/MondayResponseConverter.java
@@ -0,0 +1,29 @@
+package os.psy.research.spotlight.domain.adpater.monday.converter;
+
+import os.psy.research.spotlight.domain.adpater.monday.mapper.GetAllBoardsMondayApiResponse;
+import os.psy.research.spotlight.domain.entity.Board;
+
+import java.util.Collections;
+import java.util.List;
+
+public class MondayResponseConverter {
+
+    private MondayResponseConverter() {
+    }
+
+    public static List<Board> convertToBoards(GetAllBoardsMondayApiResponse response) {
+        if (response == null || response.getData() == null)
+            return Collections.emptyList();
+        return response.getData().getBoards().stream()
+                .map(MondayResponseConverter::convertToBoards)
+                .toList();
+    }
+
+    private static Board convertToBoards(GetAllBoardsMondayApiResponse.Data.Board response) {
+        return Board.builder()
+                .boardId(String.valueOf(response.getId()))
+                .boardName(response.getName())
+                .build();
+    }
+
+}
