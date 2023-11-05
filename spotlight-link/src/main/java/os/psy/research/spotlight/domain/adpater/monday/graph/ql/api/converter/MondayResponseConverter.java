f863d460586864052d304f59148df9f4cecd6b6d
Varranz <varranz.devs@outlook.com>
2023-11-05 21:31:59 +0100

    LK-41 : Refactor Mapper Classes

--- spotlight-link/src/main/java/os/psy/research/spotlight/domain/adpater/monday/graph/ql/api/converter/MondayResponseConverter.java
+++ spotlight-link/src/main/java/os/psy/research/spotlight/domain/adpater/monday/graph/ql/api/converter/MondayResponseConverter.java
@@ -1,6 +1,6 @@
 package os.psy.research.spotlight.domain.adpater.monday.graph.ql.api.converter;
 
-import os.psy.research.spotlight.domain.adpater.monday.graph.ql.api.mapper.RawBoards;
+import os.psy.research.spotlight.domain.adpater.monday.graph.ql.api.mapper.GetAllBoardsResponse;
 import os.psy.research.spotlight.domain.entity.Board;
 
 import java.util.List;
@@ -10,13 +10,12 @@ public class MondayResponseConverter {
     private MondayResponseConverter() {
     }
 
-    public static List<Board> convertToBoards(RawBoards response) {
-        return response.getData().getBoards().stream()
-                .map(MondayResponseConverter::convertToBoards)
+    public static List<Board> convertToBoards(GetAllBoardsResponse response) {
+        return response.getData().getBoards().stream().map(MondayResponseConverter::convertToBoards)
                 .toList();
     }
 
-    private static Board convertToBoards(RawBoards.Data.Board response) {
+    private static Board convertToBoards(GetAllBoardsResponse.Data.Board response) {
         return Board.builder()
                 .boardId(String.valueOf(response.getId()))
                 .boardName(response.getName())
